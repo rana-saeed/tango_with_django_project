2e984705c04c6b823d9b70d481c66a02b65d009b
Rana Elsheikh <ranas.2122@gmail.com>
2022-02-24 15:33:22 +0000

    deleted test files

--- rango/admin.py
+++ rango/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from rango.models import Category, Page, UserProfile
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ("title", 'category', 'url')
+    list_display = ('title', 'category', 'url')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
